2877a1fca77cf82126bef2dfa863dbda99342cab
Joz <joz@showcaseimagery.com>
2017-06-19 18:29:44 +0100

    speed improvements

--- index.php
+++ index.php
@@ -53,7 +53,9 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
   <!-- Bootstrap core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom styles for this template -->
-  <link href="css/font-awesome.css" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <link href="css/animate.css" rel="stylesheet" />
   <link href="css/style.css" rel="stylesheet">
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
